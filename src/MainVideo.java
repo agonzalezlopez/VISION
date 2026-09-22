f81d682c23f39f80076cbaf3ffc2c3fef34adf6a
Andrew Gonzalez Lopez <andrewgonzalezlopez312@gmail.com>
2026-09-22 16:09:30 -0700

    Rechanged back
    There are other factors to consider instead of mainly focusing on the video displayed

--- src/MainVideo.java
+++ src/MainVideo.java
@@ -3,7 +3,6 @@ import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.videoio.VideoCapture;
 import org.opencv.core.Point;
-import org.opencv.imgproc.CLAHE;
 
 import javax.sound.sampled.Line;
 import javax.swing.*;
@@ -30,7 +29,7 @@ public class MainVideo extends JFrame{
 
     private static boolean saved = false; // One image saved instead of 100 in one press
 
-    private static final double MIN_AREA = 5000; // subject to change for webcam resolution
+    private static final double MIN_AREA = 100;
 
     public MainVideo(){
         panel = new JPanel();
@@ -82,6 +81,9 @@ public class MainVideo extends JFrame{
 
         System.out.println("check2");
 
+        // Morph_Rect, in order to create a rectangular Kernel space
+        Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5,5));
+
         if(!vid.isOpened()){
             System.out.println("Failed");
         }
@@ -90,37 +92,21 @@ public class MainVideo extends JFrame{
             System.out.println("In thread");
             MatOfByte matB = new MatOfByte(); // turns Mat into bytes
             MatOfByte matGray = new MatOfByte();
-
-            //only needs to be created once
-            CLAHE clahe = Imgproc.createCLAHE(2.0, new Size(8,8));
-
-            // Morph_Rect, in order to create a rectangular Kernel space and Morph_close to erase noise
-            Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(9,9));
             while(vid.isOpened()){
                 if(vid.read(mats)){
 
                     Core.flip(mats,mats,+1); // flips image as a reflection instead of inverted
                     Imgproc.cvtColor(mats,grayScale,Imgproc.COLOR_BGR2GRAY);
-                    clahe.apply(grayScale, grayScale);
                     Imgproc.GaussianBlur(grayScale,blurScale,new Size(5,5),0);
-
-                    MatOfDouble mean = new MatOfDouble();
-                    MatOfDouble stddev = new MatOfDouble();
-                    Core.meanStdDev(blurScale, mean, stddev);
-                    double median = mean.get(0,0)[0]; // approx; for true median you'd sort pixel values
-                    double sigma = 0.33;
-                    double lower = Math.max(0, (1.0 - sigma) * median); // lower bound for brightness
-                    double upper = Math.min(255, (1.0 + sigma) * median); // upper bound for brightness
-                    System.out.println("lower: " + lower + " upper: " + upper);
-
-                    Imgproc.Canny(blurScale,edgeScale,lower,upper);
+                    Imgproc.Canny(blurScale,edgeScale,40,120);
                     System.out.println("loading");
 
                     // Will store all the points that are graphed
                     List<MatOfPoint> contours = new ArrayList<>();
 
 
-                    Imgproc.dilate(edgeScale, edgeScale, kernel); // increase size to kernel in order to avoid finger blocking
+                    Imgproc.dilate(edgeScale,edgeScale,kernel);
+                    //Erases Noise
                     Imgproc.morphologyEx(edgeScale,edgeScale,Imgproc.MORPH_CLOSE,kernel);
 
                     Mat hierarchy = new Mat();
@@ -138,18 +124,20 @@ public class MainVideo extends JFrame{
                         double contourPerimeter = Imgproc.arcLength(contourVectors,true);
 
                         MatOfPoint2f approx = new MatOfPoint2f();
+
+                        //Epsilon = points, greater value = fewer points, lower value = more points on mat
                         Imgproc.approxPolyDP(contourVectors,approx,0.06 * contourPerimeter,true);
 
                         MatOfPoint approxInt = new MatOfPoint();
                         approx.convertTo(approxInt, CvType.CV_32S);
 
-                        RotatedRect rectBound = Imgproc.minAreaRect(approx);
-                        double rectArea = rectBound.size.height * rectBound.size.width;
+                        Rect rectBound = Imgproc.boundingRect(approx);
+                        double rectArea = rectBound.height * rectBound.width;
 
                         System.out.println("APPROX TOTAL : " + approx.total());
 
-                        //Keep within 4 to 6 vertices since approx picks up multiple vertices despite
-                        if(rectArea >= MIN_AREA && 6 >= approx.total() && approx.total() >= 4){
+                        //EXACTLY 4 points
+                        if(rectArea >= MIN_AREA && approx.total() == 4){
                             System.out.println("Convex? : " + Imgproc.isContourConvex(approxInt));
                             List<MatOfPoint> validContour = new ArrayList<>();
                             validContour.add(m);
@@ -159,9 +147,9 @@ public class MainVideo extends JFrame{
                             Point[] points = approx.toArray();
                             int edgeCounter = 0;
                             for(int i = 0; i < points.length; i++){
-                                Point pointA = points[(i-1 + 4)%points.length];
+                                Point pointA = points[(i-1 + 4)%4];
                                 Point pointB = points[i];
-                                Point pointC = points[(i+1 + 4)%points.length];
+                                Point pointC = points[(i+1 + 4)%4];
 
                                 Point vectorAB = new Point(pointA.x-pointB.x,pointA.y-pointB.y);
                                 Point vectorCB = new Point(pointC.x-pointB.x, pointC.y-pointB.y);
@@ -176,21 +164,14 @@ public class MainVideo extends JFrame{
                                 }
                                 System.out.println("Discrepancy value" + discrepancy);
                             }
-                            boolean isPerp = (edgeCounter >= 2);
+                            boolean isPerp = (edgeCounter >= 3);
                             System.out.println(isPerp);
 
 //                            double fillArea = actualArea/rectArea;
 
                             if(Imgproc.isContourConvex(approxInt) && isPerp) {
-                                Point[] boxPoints = new Point[4];
-                                rectBound.points(boxPoints); // fills the array with the 4 corners of the rotated box
-
-                                MatOfPoint boxMat = new MatOfPoint(boxPoints);
-                                List<MatOfPoint> boxList = new ArrayList<>();
-                                boxList.add(boxMat);
-
-                                // Can now do rotated boxes
-                                Imgproc.drawContours(mats,boxList, 0, new Scalar(0, 0, 255));
+                                Imgproc.drawContours(mats,validContour, 0, new Scalar(0, 0, 255));
+                                Imgproc.rectangle(mats, rectBound, new Scalar(255, 0, 0),5);
                             }
                         }
                     }
