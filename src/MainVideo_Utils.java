fa2edc366f19adebc406c4ca965dd1042422db06
Andrew Gonzalez Lopez <andrewgonzalezlopez312@gmail.com>
2026-09-23 23:22:34 -0700

    Added Marking contours
    Creates a list of contours and filters them out by area and marks them on the Mat

--- src/MainVideo_Utils.java
+++ src/MainVideo_Utils.java
@@ -1,7 +1,9 @@
-import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Size;
+import org.opencv.core.*;
 import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * This Class will contain all the data processing functions and will handle Mats accordingly
@@ -28,5 +30,34 @@ public class MainVideo_Utils {
         return processedMat;
     }
 
+    public static boolean markEdgeContours(final Mat processsedMat, final Mat originalMat){
+        ArrayList<MatOfPoint> allcontours = new ArrayList<>();
+
+        //Finds contours using kernel and uses uncompressed data(use chain simple later)
+        Imgproc.findContours(processsedMat,allcontours,new Mat(processsedMat.rows(),processsedMat.cols(),processsedMat.type()),Imgproc.RETR_EXTERNAL,Imgproc.CHAIN_APPROX_NONE);
+
+        // fucntion that returns true or false given that it contains only contours,
+        // the boolean is determined through a test ()
+        final List<MatOfPoint> filterContours = allcontours.stream()
+                .filter(contour -> {
+                    double contourArea = Imgproc.contourArea(contour);
+                    Rect boundingRect = Imgproc.boundingRect(contour);
+
+                    boolean isNoise = (contourArea>=1000);
+                    if(isNoise){
+                        Imgproc.putText(originalMat,"Area: " + contourArea,
+                                new Point(boundingRect.x + boundingRect.width, boundingRect.y + boundingRect.height),2,2,new Scalar(0,0,255));
+                        MatOfPoint2f distance = new MatOfPoint2f();
+                        contour.convertTo(distance,CvType.CV_32F);
+                        Imgproc.approxPolyDP(distance,distance,0.02*Imgproc.arcLength(distance,true),true);
+
+                        //apply put text feature here
+                    }
+
+                    return isNoise;
+                }).toList();
+
+        return false;
+    }
 
 }
